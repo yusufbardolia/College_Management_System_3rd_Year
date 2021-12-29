d91a27058cbf42fc7620d5b500cc72701f0b8eea
yusufbardolia <73817440+yusufbardolia@users.noreply.github.com>
2021-12-29 16:40:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -75,6 +75,25 @@
  } else {
  	echo "<h3>No one is currently registered.</h3>";
  }
+ $sql_select = "SELECT * FROM registration_tbl";
+ $stmt = $conn->query($sql_select);
+ $registrants = $stmt->fetchAll(); 
+ if(count($registrants) > 0) {
+ 	echo "<h2>People who are registered:</h2>";
+ 	echo "<table>";
+ 	echo "<tr><th>Name</th>";
+ 	echo "<th>Email</th>";
+ 	echo "<th>Date</th></tr>";
+ 	foreach($registrants as $registrant) {
+ 		echo "<tr><td>".$registrant['name']."</td>";
+ 		echo "<td>".$registrant['email']."</td>";
+ 		echo "<td>".$registrant['date']."</td></tr>";
+     }
+  	echo "</table>";
+ } else {
+ 	echo "<h3>No one is currently registered.</h3>";
+ }
+
 
  ?>
  </body>
