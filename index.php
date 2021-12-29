03d6b19672643bfb16120f5157a3852f11e5173e
yusufbardolia <73817440+yusufbardolia@users.noreply.github.com>
2021-12-29 17:17:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,56 @@
        <input type="submit" name="submit" value="Submit" />
  </form>
  <?php
-
+ // DB connection info
+ $host = "yusuf-database-server.database.windows.net";
+ $user = "yusuf";
+ $pwd = "bardolia@8378";
+ $db = "yusuf sql database";
+ // Connect to database.
+ try {
+ 	$conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+ 	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+ }
+ catch(Exception $e){
+ 	die(var_dump($e));
+ }
+ if(!empty($_POST)) {
+ try {
+ 	$name = $_POST['name'];
+ 	$email = $_POST['email'];
+ 	$date = date("Y-m-d");
+ 	// Insert data
+ 	$sql_insert = "INSERT INTO registration_tbl (name, email, date) 
+ 				   VALUES (?,?,?)";
+ 	$stmt = $conn->prepare($sql_insert);
+ 	$stmt->bindValue(1, $name);
+ 	$stmt->bindValue(2, $email);
+ 	$stmt->bindValue(3, $date);
+ 	$stmt->execute();
+ }
+ catch(Exception $e) {
+ 	die(var_dump($e));
+ }
+ echo "<h3>Your're registered!</h3>";
+ }
+ $sql_select = "SELECT * FROM registration_tbl";
+ $stmt = $conn->query($sql_select);
+ $registrants = $stmt->fetchAll(); 
+ if(count($registrants) > 0) {
+ 	echo "<h2>People who are registered:</h2>";
+ 	echo "<table>";
+ 	echo "<tr><th>Name</th>";
+ 	echo "<th>Email</th>";
+ 	echo "<th>Date</th></tr>";
+ 	foreach($registrants as $registrant) {
+ 		echo "<tr><td>".$registrant['name']."</td>";
+ 		echo "<td>".$registrant['email']."</td>";
+ 		echo "<td>".$registrant['date']."</td></tr>";
+     }
+  	echo "</table>";
+ } else {
+ 	echo "<h3>No one is currently registered.</h3>";
+ }
 
 
  ?>
