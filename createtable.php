d078276acaffb9fe5b0b33798dac2ce30c1a8a04
yusufbardolia <73817440+yusufbardolia@users.noreply.github.com>
2021-12-29 13:46:50 +0530

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -7,6 +7,9 @@
  try{
  	$conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
  	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  catch(Exception $e){
+ 	die(var_dump($e));
+ }
  	$sql = "CREATE TABLE registration_tbl(
  	id INT NOT NULL IDENTITY(1,1) 
  	PRIMARY KEY(id),
